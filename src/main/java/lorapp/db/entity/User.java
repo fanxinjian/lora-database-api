2cc9597ccf718f42c8072871870919e455d6878c
MoseChen <864706976@qq.com>
2016-12-12 00:59:37 +0800

    Add userRole property for User entity

--- src/main/java/lorapp/db/entity/User.java
+++ src/main/java/lorapp/db/entity/User.java
@@ -4,6 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * @Author MoseC
@@ -16,14 +20,27 @@ public class User {
     @GeneratedValue
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 10)
+    @NotNull
+    @Size(min = 1, max = 10, message = "用户名长度必须在1~10之间")
     private String userName;
 
+    @NotNull
+    @Size(min = 5, max = 10, message = "密码长度必须在5~10之间")
     @Column(nullable = false, length = 50)
     private String passwd;
 
+    /**
+     * 管理员:admin, 普通用户:ordin
+     */
+    @Column(nullable = false, length = 20)
+    private String role = USER_ORDIN;
+
     @Column(length = 100)
     private String description;
+
+    public static final String USER_ADMIN = "admin";
+    public static final String USER_ORDIN = "ordin";
 
 
     public User(){}
@@ -65,5 +82,13 @@ public class User {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }

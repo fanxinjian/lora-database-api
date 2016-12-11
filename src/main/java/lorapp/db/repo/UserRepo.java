e36d61e853cf43e4fe5e758fa853dc8d403c353d
Tristan Yu <Tristan.Yu@alcatel-lucent.com>
2016-12-11 21:20:02 +0800

    Remove warnings

--- src/main/java/lorapp/db/repo/UserRepo.java
+++ src/main/java/lorapp/db/repo/UserRepo.java
@@ -1,11 +1,9 @@
 package lorapp.db.repo;
 
-import lorapp.db.entity.AppEUI;
-import lorapp.db.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import lorapp.db.entity.User;
 
 @Transactional
 
